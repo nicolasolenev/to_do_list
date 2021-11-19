73137dc535350d532dee31b0b0fd4c6b9881b693
Nicolas Olenev <cardinal2113@gmail.com>
2021-11-19 10:28:45 +0300

    set-interval 2.0

--- some_interval.js
+++ some_interval.js
@@ -1,11 +1,13 @@
 function printNumbers(from, to) {
   function someFunction() {
-    if (from <= to) {
-      console.log(from);
-    }
+    console.log(from);
     from++;
+    if (from > to) {
+      clearInterval(sdf);
+    }
   }
-  setInterval(someFunction, 1000);
+  let sdf = setInterval(someFunction, 1000);
+
 }
 
 printNumbers(1, 5);
