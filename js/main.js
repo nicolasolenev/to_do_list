6968044daba652d15cb0a9d7523ddd81dd5fbe5b
Nicolas Olenev <cardinal2113@gmail.com>
2022-01-24 05:56:35 +0300

    add storage and constructor

--- js/main.js
+++ js/main.js
@@ -1,22 +1,88 @@
-const list = [];
-const STATUSES = ['To Do', 'In Progress', 'Done'];
+import { taskHtmlElementTemplate, deleteTaskBtnHandler, checkboxTaskHandler } from './view.js'
+
+let list = [];
+const TODO = 'To Do';
+const IN_PROGRESS = 'In Progress';
+const DONE = 'Done'
+const STATUSES = [TODO, IN_PROGRESS, DONE];
 let ID = 0;
 
+renderList();
+
+// localStorage.clear();
+
+if (localStorage.getItem('list')) {
+  list = JSON.parse(localStorage.getItem('list'));
+} else {
+  localStorage.setItem('list', '[]');
+}
+
+if (localStorage.getItem('ID')) {
+  ID = JSON.parse(localStorage.getItem('ID'));
+} else {
+  localStorage.setItem('ID', '0');
+}
+
+function saveToLocalStorage(task) {
+  let list = JSON.parse(localStorage.getItem('list'));
+  list.push(task);
+  localStorage.setItem('list', JSON.stringify(list));
+  localStorage.setItem('ID', ID);
+}
+
+function deleteFromLocalStorage(ID) {
+  let list = JSON.parse(localStorage.getItem('list'));
+  let map = new Map(list.map(item => [item.id, item]));
+  map.delete(ID);
+  localStorage.setItem('list', JSON.stringify([...map.values()]));
+}
+
+function renderList() {
+  let list = JSON.parse(localStorage.getItem('list'));
+  for (let task of list) {
+    const whereAddTask = document.getElementById(task.priority);
+    const newTaskHtmlElement = taskHtmlElementTemplate.cloneNode('deep');
+    newTaskHtmlElement.setAttribute('id', task.id);
+    const taskNameTextElement = newTaskHtmlElement.querySelector('.todo_list__input');
+    taskNameTextElement.textContent = task.name;
+    const deleteBtnElement = newTaskHtmlElement.querySelector('.todo_list__delete_button');
+    deleteBtnElement.addEventListener('click', deleteTaskBtnHandler);
+    const checkboxElement = newTaskHtmlElement.querySelector('.todo_list__checkbox');
+    checkboxElement.addEventListener('click', checkboxTaskHandler);
+
+    if (task.check) {
+      newTaskHtmlElement.classList.add('Done');
+      checkboxElement.previousElementSibling.classList.remove('display_none');
+    }
+    whereAddTask.append(newTaskHtmlElement);
+  }
+}
+
+function Task(task, priority) {
+  this.id = ++ID;
+  this.name = task;
+  this.status = 'To Do';
+  this.priority = priority;
+  this.check = false;
+}
+
 function addTask(task, priority = 'low') {
-  list.push({
-    id: ++ID,
-    name: task,
-    status: 'To Do',
-    priority: priority,
-  });
+  let newTask = new Task(task, priority);
+  list.push(newTask);
+  saveToLocalStorage(newTask);
 }
 
 function changeStatus(ID, status) {
-  list.find(task => task.id === ID).status = status;
+  let list = JSON.parse(localStorage.getItem('list'));
+  let task = list.find(item => item.id === ID);
+  task.check = task.check ? false : true;
+  list[list.findIndex(item => item.id === ID)] = task;
+  localStorage.setItem('list', JSON.stringify(list));
 }
 
 function deleteTask(ID) {
   list.splice(list.findIndex(task => task.id === ID), 1);
+  deleteFromLocalStorage(ID);
 }
 
 function showList() {
@@ -32,4 +98,11 @@ function showList() {
   }
 }
 
-export { list, ID, addTask, deleteTask, changeStatus };
+export { list, ID, addTask, deleteTask, changeStatus, TODO, IN_PROGRESS, DONE };
+
+export const UI_ELEMENTS = {
+  ADD_TASK: document.querySelectorAll('.todo__add-icon'),
+  CHECKED: document.querySelectorAll('.todo__checkbox'),
+  TASK_CLONE: document.querySelector('.todo__task'),
+  INPUT: document.querySelectorAll('.todo__add'),
+};
--- js/view.js
+++ js/view.js
@@ -1,65 +1,77 @@
-import { list, ID, addTask, deleteTask, changeStatus } from './main.js'
+import { list, ID, addTask, deleteTask, changeStatus, TODO, IN_PROGRESS, DONE, UI_ELEMENTS } from './main.js'
 
-const taskTemplate = document.createElement('div');
-taskTemplate.className = 'todo_list__task';
-taskTemplate.insertAdjacentHTML('afterbegin', `
+const taskHtmlElementTemplate = document.createElement('div');
+taskHtmlElementTemplate.className = 'todo_list__task';
+taskHtmlElementTemplate.insertAdjacentHTML('afterbegin', `
+<span class="circle display_none"></span>
 <input type="checkbox" class="todo_list__checkbox">
 <span class="todo_list__input"></span>
 <button class="todo_list__delete_button">
   <img class="todo_list__delete_button_icon" src="img/add-icon.svg">
 </button>`);
 
-const addTaskBtns = document.querySelectorAll('.todo_list__add_button');
-for (let button of addTaskBtns) {
-  button.addEventListener('click', addTaskHandler);
+const addNewTaskBtns = document.querySelectorAll('.todo_list__add_button');
+for (let addNewTaskBtn of addNewTaskBtns) {
+  addNewTaskBtn.addEventListener('click', addNewTaskBtnHandler);
 }
 
-function addTaskHandler(event) {
-  const whereAddTask = event.currentTarget.parentElement.parentElement;
-  const taskName = event.currentTarget.parentElement.firstElementChild.value;
-  const isNotEmptyField = (taskName.trim() !== '');
-  if (isNotEmptyField) {
-    event.currentTarget.parentElement.firstElementChild.value = null;
-    const priority = event.currentTarget.parentElement.parentElement.firstElementChild.textContent;
-    addTask(taskName, priority);
-    const newTask = taskTemplate.cloneNode('deep');
-    newTask.setAttribute('id', ID);
-    newTask.querySelector('.todo_list__input').textContent = taskName;
-    const deleteBtn = newTask.querySelector('.todo_list__delete_button');
-    deleteBtn.addEventListener('click', deleteTaskHandler);
-    const checkbox = newTask.querySelector('.todo_list__checkbox');
-    checkbox.addEventListener('click', checkboxTaskHandler);
-    whereAddTask.append(newTask);
-  }
-}
-
-function deleteTaskHandler() {
-  const taskID = +this.parentElement.id;
-  deleteTask(taskID);
-  this.parentElement.remove();
-}
-
-function checkboxTaskHandler() {
-  const taskID = +this.parentElement.id;
-  const task = this.parentElement;
-  if (task.classList.contains('done')) {
-    changeStatus(taskID, 'In Progress');
-    task.classList.remove('done');
-    task.parentElement.firstElementChild.nextElementSibling.after(task);
-  } else {
-    changeStatus(taskID, 'Done');
-    task.parentElement.querySelector('.done') ? task.parentElement.querySelector('.done').before(task) :
-      task.parentElement.append(task);
-    task.classList.add('done');
-  }
-}
-
-const inputs = document.querySelectorAll('.todo_list__input');
-for (let input of inputs) {
-  input.addEventListener('keydown', function (key) {
-    const isEnterBtn = (key.keyCode === 13);
-    if (isEnterBtn) {
-      addTaskHandler(event);
+function addNewTaskBtnHandler(event) {
+  const inputField = event.currentTarget.parentElement.firstElementChild;
+  const taskName = inputField.value;
+  const isNotEmptyInputField = (taskName.trim() !== '');
+  try {
+    if (!isNotEmptyInputField) {
+      throw new SyntaxError("Нельзя создать пустую задачу");
     }
-  });
+    inputField.value = '';
+    const whereAddTask = event.currentTarget.parentElement.parentElement;
+    const priority = whereAddTask.firstElementChild.textContent;
+    addTask(taskName, priority);
+    const newTaskHtmlElement = taskHtmlElementTemplate.cloneNode('deep');
+    newTaskHtmlElement.setAttribute('id', ID);
+    const taskNameTextElement = newTaskHtmlElement.querySelector('.todo_list__input');
+    taskNameTextElement.textContent = taskName;
+    const deleteBtnElement = newTaskHtmlElement.querySelector('.todo_list__delete_button');
+    deleteBtnElement.addEventListener('click', deleteTaskBtnHandler);
+    const checkboxElement = newTaskHtmlElement.querySelector('.todo_list__checkbox');
+    checkboxElement.addEventListener('click', checkboxTaskHandler);
+    whereAddTask.append(newTaskHtmlElement);
+  } catch (e) {
+    if (e.name == "SyntaxError") {
+      const body = document.body;
+      const shield = document.createElement('div');
+      shield.style.height = '100%';
+      shield.style.width = '100%';
+      shield.zIndex = '9000';
+      body.append(shield);
+      // alert(e.message);
+    } else {
+      throw e; // проброс (*)
+    }
+  }
 }
+
+function deleteTaskBtnHandler(event) {
+  const taskID = getTaskID(event);
+  deleteTask(taskID);
+  event.currentTarget.parentElement.remove();
+}
+
+function checkboxTaskHandler(event) {
+  event.currentTarget.previousElementSibling.classList.toggle('display_none');
+  const taskID = getTaskID(event);
+  const task = event.currentTarget.parentElement;
+  if (task.classList.contains(DONE)) {
+    changeStatus(taskID, IN_PROGRESS);
+    task.classList.remove(DONE);
+  } else {
+    changeStatus(taskID, DONE);
+    task.classList.add(DONE);
+  }
+}
+
+function getTaskID(event) {
+  return +event.currentTarget.parentElement.id;
+}
+
+export { taskHtmlElementTemplate, deleteTaskBtnHandler, checkboxTaskHandler }
